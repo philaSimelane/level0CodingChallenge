d3de877981f6fbdeb14e660bd6f579757ef4ddd1
philaSimelane <nokphilasimelane@gmail.com>
2022-01-14 03:48:17 +0200

    Added Task03

---
+++ task03.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		System.out.println(Hello("Tshepo"));
+		
+	}
+	static String Hello(String name){
+        return "Hello " + name + "!";
+    }
+
+}
