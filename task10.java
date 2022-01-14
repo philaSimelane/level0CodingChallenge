c561c313abd4555ce7c909595ab57affc08f2a17
philaSimelane <nokphilasimelane@gmail.com>
2022-01-14 04:19:03 +0200

    Added Task10

---
+++ task10.java
@@ -0,0 +1,19 @@
+public class Main {
+	public static void main(String[] args) {
+		commonCharacters("house", "computers");
+		
+	}
+	static void commonCharacters(String word1, String word2) {
+		System.out.print("Common leters: ");
+		for (int i = 0; i < word1.length(); i++) {
+			for (int j= 0; j < word2.length(); j++) {
+			
+				if (word1.charAt(i) == word2.charAt(j)) {
+					System.out.print(word1.charAt(i) + " ");
+				}
+			}
+		}
+	}	
+			
+		
+}
