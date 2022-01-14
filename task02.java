fb4060dd4ff6dacf37eb68654250316648945e4b
philaSimelane <nokphilasimelane@gmail.com>
2022-01-14 03:39:49 +0200

    Ade d
    q
    
    cd ..//

---
+++ task02.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		double x = 1 + 1 * 2;
+		double y = (1 + 1) * 2;
+		double z = 1 + ( 1 * 2 );
+		double a = 1+ 1 * 2 /2;
+		double b = (1+ 1 * 2 ) /2.0;
+		System.out.println((int)x);
+		System.out.println((int)y);
+		System.out.println((int)z);
+		System.out.println((int)a);
+		System.out.println(b);
+	}
+}
