b2a4cf6d410bb96fd0ec730dc96db4111519da7f
philaSimelane <nokphilasimelane@gmail.com>
2022-01-14 03:25:44 +0200

    Added Task01

---
+++ task01.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args){
+		int x = 0;
+		int y = 1;
+		System.out.println(x);
+		System.out.println(y);
+		x = x + 3;
+		y = y + x;
+		System.out.println(x);
+		System.out.println(y);
+	}
+
+
+	}
