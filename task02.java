b4bd66ed1166e33fe818101a9474c561505e43aa
philaSimelane <86529871+philaSimelane@users.noreply.github.com>
2022-01-14 03:58:34 +0200

    Added Task02

--- task02.java
+++ task02.java
@@ -3,8 +3,8 @@ public class Main {
 		double x = 1 + 1 * 2;
 		double y = (1 + 1) * 2;
 		double z = 1 + ( 1 * 2 );
-		double a = 1+ 1 * 2 /2;
-		double b = (1+ 1 * 2 ) /2.0;
+		double a = 1 + 1 * 2 /2;
+		double b = (1 + 1 * 2 ) /2.0;
 		System.out.println((int)x);
 		System.out.println((int)y);
 		System.out.println((int)z);
