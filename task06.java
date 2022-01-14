a80cad3df347d1e8641922792fb8b29882127d1e
philaSimelane <nokphilasimelane@gmail.com>
2022-01-15 01:47:56 +0200

    Modified Task06

--- task06.java
+++ task06.java
@@ -4,12 +4,12 @@ public class Main {
 		
 	}
 	static int maximum(int num1, int num2, int num3) {
-		if (num1 > num2 && num1 > num3)
-			return num1;
-		else if (num2 > num1&& num2 > num3)
-			return num2;
-		else 
-			return num3;
+				if (num1 > num2 && num1 > num3)
+					return num1;
+				else if (num2 > num1&& num2 > num3)
+					return num2;
+				else
+					return num3;
 	}
 
 		
