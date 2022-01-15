a27d1a599c2398aa666fdc7d2cf23f43c09fecf4
philaSimelane <nokphilasimelane@gmail.com>
2022-01-15 03:30:31 +0200

    Modified Task10

--- task10.java
+++ task10.java
@@ -4,12 +4,18 @@ public class Main {
 		
 	}
 	static void commonCharacters(String word1, String word2) {
-		System.out.print("Common leters: ");
+		System.out.print("Common letters: ");
 		for (int i = 0; i < word1.length(); i++) {
 			for (int j= 0; j < word2.length(); j++) {
 			
 				if (word1.charAt(i) == word2.charAt(j)) {
-					System.out.print(word1.charAt(i) + " ");
+					StringBuilder builder= new StringBuilder();
+					builder.append(word1.charAt(i));
+					if ( word1.indexOf(word1.charAt(i)) != word1.length() -1){
+        	builder.append(", ");
+        			}
+					System.out.print(builder.toString());
+					
 				}
 			}
 		}
